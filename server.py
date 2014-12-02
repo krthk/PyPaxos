ab332134e79b1cec339a9ecad5f2a673b6b47576
Jonathan Brown <jbrownie77@gmail.com>
2014-12-02 10:59:56 -0800

    UDP server updated

--- server.py
+++ server.py
@@ -14,30 +14,15 @@ class Server(object):
         self.port = 55555
         self.bufferSize = 1024
         self.server = None
-        self.threads = []
         
         self.isRunning = True
     
     
     #Networking setup
     def listen(self):
-        self.open_socket()
-        
-        while True:
-            if self.isRunning:
-                #Establish connection with client
-                client, addr = self.server.accept()
-            
-                #Dispatch thread to handle client
-                threading.Thread(target=self.clientHandler, args=[client, addr])
-            
-
-
-    #Handle opening server socket
-    def open_socket(self):
         try:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.server.bind((self.host,self.port))
+            self.server.bind((self.host, self.port))
         
         except socket.error, (value,message):
             if self.server:
@@ -47,28 +32,11 @@ class Server(object):
             sys.exit(1)
 
         while True:
-            data, addr = sock.recvfrom(self.bufferSize)
-            print "received message:", data
-
-
-    #Threaded handler for clients
-    def clientHandler(self, client, addr):
-        print "Got connection from" + addr
-        
-        while True:
+            data, addr = self.server.recvfrom(self.bufferSize)
+            
             if self.isRunning:
-                data = client.recv(self.size)
-                
-                if not data:
-                    break
+                print "Received data:", data
 
-                client.send(data)
-    
-            else:
-                time.sleep(1)
-        
-        client.close()
-    
     
     #Stop all network activity
     def fail(self):
