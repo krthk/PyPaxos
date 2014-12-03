e3f61383caefdf6d9cfc6d525fc47c0687c8806b
Jonathan Brown <jbrownie77@gmail.com>
2014-12-03 12:18:32 -0800

    Changed paxos object to round

---
+++ paxos/round.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import socket
+import threading
+from message import MessageType
+from message import Message
+
+
+class Round(threading.Thread):
+    
+    #Class "constructor"
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.localIP = ''
+        self.localPort = 55555
+        self.socket = None
+        self.ballot = Ballot(self.localIP, self.localPort)
+    
+    
+    #Networking setup
+    def setup(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+        except socket.error, (value, message):
+            if self.socket:
+                self.socket.close()
+            
+            print "Could not open socket: " + message
+            sys.exit(1)
+
+
+    #Send to some server
+    def send(self, message, ip, port):
+        if self.isRunning:
+            data = pickle.dumps(message)
+                self.socket.sendto(data, (ip, port))
+
+
+    #Begin "Propose" phase
+    def propose(self, messageType):
+        message = Message(MessageType.Propose, self.ballot)
+        
+        #CHANGE THIS TO SEND TO NETWORK
+        self.send(message, self.localIP, self.localPort)
