9b5acb671ea97b6f90e5323f06757041c7ef9312
Karthik Puthraya <karthikputhraya@gmail.com>
2014-12-03 17:29:18 -0800

    Fix message pump errors

--- paxos/messagepump.py
+++ paxos/messagepump.py
@@ -1,7 +1,7 @@
 '''
-    Created on Dec 3, 2014
-    @author: karthik
-    '''
+Created on Dec 3, 2014
+@author: karthik
+'''
 import threading
 import Queue
 import sys
@@ -10,15 +10,15 @@ import time
 
 class MessagePump(threading.Thread):
     '''
-        This class listens to a port on a node and passes the messages it receives to
-        the main thread.
-        '''
+    This class listens to a port on a node and passes the messages it receives to
+    the main thread.
+    '''
     
-    def __init__(self, owner, port, queue):
+    def __init__(self, queue, owner = None, port = 55555):
         '''
-            The MessagePump binds itself to port and appends all the messages it receives
-            to a queue
-            '''
+        The MessagePump binds itself to port and appends all the messages it receives
+        to a queue
+        '''
         threading.Thread.__init__(self)
         self.owner = owner
         self.port = port
@@ -27,11 +27,11 @@ class MessagePump(threading.Thread):
         self.isRunning = True
     
     def run(self):
-	print "Starting message pump"
+	print 'Starting message pump and listerning to port ', self.port
 
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.socket.bind(("localhost", self.port))
+            self.socket.bind(('localhost', self.port))
             self.isRunning = True
         
             #self.socket.settimeout( timeout )
@@ -40,8 +40,8 @@ class MessagePump(threading.Thread):
             if self.socket:
                 self.socket.close()
             
-            print "Could not open/bind to socket."
-            print "Exception message:", e
+            print 'Could not open/bind to socket.'
+            print 'Exception message: ', e
             sys.exit(1)
         
         # Listen forever on the port and add received messages to the queue
@@ -49,3 +49,17 @@ class MessagePump(threading.Thread):
             if self.isRunning:
                 data, addr = self.socket.recvfrom(2048)                
                 self.queue.put(data)
+    
+if __name__ == '__main__':
+    queue = Queue.Queue()
+    mp = MessagePump(queue)
+    mp.setDaemon(True)
+    mp.start()
+    
+    while True:
+        if not queue.empty():
+            data = queue.get()
+            print data
+        time.sleep(5)
+        
+    
