448c41030e94484e25a30d053224e0b5460480ef
krthk <karthik@puthraya.com>
2014-12-06 15:21:14 -0800

    Added capturing Ctrl+C to terminate the process

--- paxos/run_node.py
+++ paxos/run_node.py
@@ -5,11 +5,20 @@ Created on Dec 5, 2014
 
 @author: Karthik Puthraya
 '''
-from sys import argv
+from sys import argv, exit
+from os import kill, getpid
 from node import Node
 from time import sleep
+import signal
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    kill(getpid(), signal.SIGTERM)
+    exit(0)
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    
     if len(argv) == 3:
         print 'Starting server at port {0}'.format(argv[2])
         n = Node(argv[1], int(argv[2]))
